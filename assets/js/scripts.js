2fea44a58943a859236b15b3149919c501572238
Marianny Díaz <mariianniidiiaz@gmail.com>
2022-09-02 23:55:05 -0500

    Añadiendo JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,30 +7,37 @@ let appointments = [];
 
 showInfo();
 
-function add(){
+function add() {
     const appointmentName = ownerInput.value;
     const appointmentDate = dateInput.value;
     appointmentsInfo.push({
-        id: Math.random(), 
-        owner: appointmentName, 
-        date: appointmentDate});
+        id: Math.random(),
+        owner: appointmentName,
+        date: appointmentDate
+    });
     console.log(appointmentDate, appointmentName);
     console.log(appointmentsInfo);
+    ownerInput.value = "";
+    dateInput.value = "";
     updateStorage();
 }
 
-function updateStorage(){
+function updateStorage() {
     localStorage.setItem("appointmentsInfo", JSON.stringify(appointmentsInfo));
     showInfo();
 }
 
-function showInfo(){
-    if (appointmentsInfo.length === 0){
+function showInfo() {
+    if (appointmentsInfo.length === 0) {
         appointmentsInput.innerHTML = '<div><h1>No appointments</h1></div>';
     } else {
         appointmentsInput.innerHTML = "";
         appointmentsInfo.map((appointment) => {
+            const section = document.createElement("section");
+            section.classList.add("appointmentsInput");
+
             const div = document.createElement("div");
+            div.classList.add("divInfo");
 
             const divName = document.createElement("h3");
             divName.innerHTML = appointment.owner;
@@ -40,11 +47,30 @@ function showInfo(){
 
             div.append(divName, divDate);
             appointmentsInput.appendChild(div);
+
+            const divButtons = document.createElement("div")
+
+            const btnEdit = document.createElement("button");
+            btnEdit.innerText = "Edit";
+            btnEdit.classList.add("divButtons");
+            btnEdit.onclick = () => editInput(appointmentsInfo);
+            divButtons.appendChild(btnEdit);
+            div.appendChild(divButtons);
+
+            const btnDelete = document.createElement("button");
+            btnDelete.innerText = "Delete";
+            btnDelete.classList.add("divButtons");
+            btnDelete.onclick = () => deleteInput(appointmentName, appointmentDate);
+            divButtons.appendChild(btnDelete);
+            div.appendChild(divButtons);
         })
     }
 }
 
-function editInput() {
-    
+function editInput(appointmentsInfo) {
+    const index = appointmentsInfo.indexOf(appointmentName, appointmentDate);
+    const newInfo = prompt("Ingresa la información: " + appointmentsInfo);
+    appointmentsInfo[index] = newInfo;
+    updateStorage();
 }
 
